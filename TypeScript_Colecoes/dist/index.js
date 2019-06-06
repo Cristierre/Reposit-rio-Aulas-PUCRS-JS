ce5b732c98f5a61e4af1722adf31f1fbddcbed33
Cristierre Konrath <cristierrekonrath95@gmail.com>
2019-06-06 17:05:40 -0300

    v3

--- TypeScript_Colecoes/dist/index.js
+++ TypeScript_Colecoes/dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 let mapa;
 mapa = new Map();
 mapa.set('RS', 'Rio Grande do Sul');
@@ -14,4 +15,34 @@ function logMapElements(value, key, map) {
     console.log(`map.get('${key}')= ${value}`);
 }
 mapa.forEach(logMapElements);
+//Exercicio 6
+function getMax(array) {
+    let maior = 0;
+    for (let valor of array) {
+        if (valor > maior) {
+            maior = valor;
+        }
+    }
+    return maior;
+}
+console.log(getMax([1, 2, 4, 2, 0]));
+console.log(getMax([0]));
+console.log(getMax([]));
+//console.log(getMax([null]));
+//console.log(getMax([undefined]));
+//Exercicio 7
+function toMax(array) {
+    let mapa = new Map();
+    array.map(num => {
+        let cont = 0;
+        for (let i = 0; i < array.length; i++) {
+            if (num === array[i]) {
+                cont++;
+            }
+        }
+        mapa.set(num, cont);
+    });
+    return mapa;
+}
+console.log(toMax([3, 4, 5, 6, 7, 7, 7, 8, 8, 8, 9, 9]));
 //# sourceMappingURL=index.js.map
--- TypeScript_Colecoes/src/index.ts
+++ TypeScript_Colecoes/src/index.ts
@@ -1,3 +1,5 @@
+import { stringify } from "querystring";
+
 let mapa: Map<string,string>;
 mapa = new Map();
 mapa.set('RS','Rio Grande do Sul');
@@ -14,3 +16,38 @@ function logMapElements(value: string, key:string, map:Map<string,string>){
     console.log(`map.get('${key}')= ${value}`);
 }
 mapa.forEach(logMapElements);
+
+//Exercicio 6
+function getMax(array: number[]): number{
+    let maior = array[0];
+    for(let valor of array){
+        if(valor>maior){
+            maior = valor;
+        }
+    }
+    return maior;
+}
+console.log(getMax([1,2,4,2,0]));
+console.log(getMax([0]));
+console.log(getMax([]));
+//console.log(getMax([null]));
+//console.log(getMax([undefined]));
+
+//Exercicio 7
+function toMax(array: number[]): Map<number,number>{
+     let mapa:Map<number, number> = new Map();
+    array.map(num =>{
+        let cont: number = 0;
+        for(let i = 0; i < array.length; i++){
+            if(num === array[i]){
+                cont++;
+            }
+            
+        }
+        mapa.set(num, cont);    
+        
+    })
+    return mapa;
+}
+console.log(toMax([3,4,5,6,7,7,7,8,8,8,9,9]));
+

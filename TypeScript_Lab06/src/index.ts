d861ce783f25f50321ba0e835112aca68cee3e5d
Cristierre <cristierrekonrath95@gmai.com>
2019-06-21 12:20:20 -0300

    Dia 21-06M

--- TypeScript_Lab06/src/index.ts
+++ TypeScript_Lab06/src/index.ts
@@ -1,8 +1,29 @@
 
 
 async function Main(){
-    const url =" https://reqres.in/"
-    let resposta: Response = await fetch()
+
+    const url ="https://reqres.in"
+    try{
+
+        let resposta: Response = await fetch(`${url}/api/users?page=2`);
+        
+        interface Post{
+            name: string,
+            email: string,
+            phone: string
+        }
+    
+        if(resposta.ok){
+          
+            let usuario:Usuario[] = await resposta.json;
+            usuario.forEach(user => console.log(user.body));
+            
+        }else{
+            console.log(`Erro: ${resposta.status}`);
+        }
+    }catch(err){
+        console.log(`Erro: ${err}`);
+    }
 
 }
 
